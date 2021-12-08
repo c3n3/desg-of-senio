f65de9a7f92a1a89ef1625857814d4c2a75a1280
Caden Churchman <ccaden21@gmail.com>
2021-12-08 15:34:13 -0600

    Added the h file

---
+++ generic_automation/exec/Website/views/Constants.h
@@ -0,0 +1,14 @@
+#ifndef __CONTANTS_H__
+#define __CONTANTS_H__
+static const std::string PRIMARY_COLOR = "#181c24";
+
+static const std::string SECONDARY_COLOR = "#343c4a";
+
+static const std::string ACCENT_COLOR = "#b52b09";
+
+static const std::string WORD_COLOR = "#ced5e0";
+
+static const std::string ANTI_PRIMARY_COLOR = "#d9ceed";
+
+static const std::string ANTI_WORD_COLOR = "#221b2e";
+#endif
