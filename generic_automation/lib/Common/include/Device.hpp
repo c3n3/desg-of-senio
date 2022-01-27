2f325cb2edc059c23a151cc6b0418e8622f0aafa
Caden Churchman <ccaden21@gmail.com>
2022-01-27 09:28:42 -0600

    Made device interface

---
+++ generic_automation/lib/Common/include/Device.hpp
@@ -0,0 +1,21 @@
+#ifndef __GENAUTO_DEVICE_HPP__
+#define __GENAUTO_DEVICE_HPP__
+
+#include <stdint.h>
+
+namespace genauto {
+    class Device {
+    public:
+        /**
+         * @brief Id for minor type for message ids
+         */
+        const uint8_t minorId;
+
+        /**
+         * @brief Do something
+         */
+        virtual void execute();
+    };
+}
+
+#endif
