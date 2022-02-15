333e70fd80ba94bc5ddbc84d6e9a8d3a99668434
Nolan Meyer <nolan144@ksu.edu>
2022-02-15 17:02:28 -0600

    stepper device class compiling and working on button device

---
+++ generic_automation/lib/Esp/include/ButtonDevice.hpp
@@ -0,0 +1,31 @@
+#ifndef __GENERIC_AUTOMATION_BUTTONDEVICE_HPP__
+#define __GENERIC_AUTOMATION_BUTTONDEVICE_HPP__
+
+#include "../../Common/include/Publisher.hpp"
+#include <stdint.h>
+
+namespace genauto {
+    /**
+     * @brief Abstract message
+     */
+    class ButtonDevice : public Publisher {
+        private:
+            uint8_t pressed_;
+    public:
+
+        /**
+         * @brief Construct a new Button Device object
+         * 
+         * @param pinNumber 
+         */
+        ButtonDevice(uint8_t pinNumber);
+
+        /**
+         * @brief executes the purpose of the Button class
+         * 
+         */
+        void execute();
+    };
+}
+
+#endif
