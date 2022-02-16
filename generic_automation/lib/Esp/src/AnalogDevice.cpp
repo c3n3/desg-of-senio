3248bb97bb70e5a91aeb2f03e165ef4e0ecd15c1
Nolan Meyer <nolan144@ksu.edu>
2022-02-16 15:51:49 -0600

    added filter to Analog Device class

--- generic_automation/lib/Esp/src/AnalogDevice.cpp
+++ generic_automation/lib/Esp/src/AnalogDevice.cpp
@@ -5,6 +5,9 @@
 using namespace genauto;
 
 unsigned long lastTime = 0;
+unsigned long readTime = 0;
+int16_t accumValue = 0  // sum of the values read in during ~ 250ms interval.
+uint8_t count = 0; // number of values summed into accumValue
 
 /**
  * @brief Construct a new genauto::Pwm Device::pwm object
@@ -15,25 +18,35 @@ genauto::AnalogDevice::AnalogDevice(uint8_t pinNumber /*, uint8_t minorId*/)
     : pinNumber(pinNumber)
 // will need to add "minorId(MinorId)"
 {
-    
+    lastTime = millis();
+    readTime = lastTime;
 }
 
 /**
- * @brief
- *
+ * @brief the function that will be called every cycle of the main loop. 
+ *          Reads in values for the analog device and filters values for a more
+ *          accurate representation of the last ~ 250ms and stores that average
+ *          in the value variable.
  */
 void genauto::AnalogDevice::execute()
 {
-    if ((millis() - lastTime) >= 250)
+    if (millis() - readTime >= 10) // read these values more often, and filter 
     {
-        lastTime = millis();
-        value = analogRead(analogReadPin);
-        flag = true;
+        accumValue += analogRead(analogReadPin);
+        count++;
+        if ((millis() - lastTime) >= 250)
+        {
+            lastTime = millis();
+            flag = true;
+            value = accumValue / count;
+            accumValue = 0;
+            count = 0;
+        }
     }
 }
 
 /**
- * @brief
+ * @brief called to return a message pointer containing the information acquired in the execute() function.
  *
  * @return Message*
  */
@@ -42,7 +55,7 @@ Message *tryGet()
     if (flag)
     {
         flag = false;
-        AnalogMessage* aMsg = AnalogMessage::AnalogMessage(void);
+        AnalogMessage *aMsg = AnalogMessage::AnalogMessage(void);
         aMsg->value() = value;
         return bMsg
     }
